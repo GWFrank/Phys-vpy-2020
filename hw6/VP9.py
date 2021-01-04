fc538e1a9a9e7fc8968a6908e3f1a13cbfe6809b
GWFrank <frank910311@gmail.com>
2021-01-04 16:34:57 +0800

    finish hw6

--- hw6/VP9.py
+++ hw6/VP9.py
@@ -26,8 +26,8 @@ dv = 10.
 for v in arange(0., 4201.+dv, dv):
     theory_low_T.plot(pos=(v, (deltav/dv)*N*4.*pi *
                            ((m/(2.*pi*k*T))**1.5)*exp((-0.5*m*v**2)/(k*T))*(v**2)*dv))
-    observation = ghistogram(graph=vdist, bins=arange(
-        0., 4200., deltav), color=color.red)
+observation = ghistogram(graph=vdist, bins=arange(
+    0., 4200., deltav), color=color.red)
 
 
 scene = canvas(width=500, height=500,
@@ -54,7 +54,20 @@ def vcollision(a1p, a2p, a1v, a2v):
     v2prime = a2v - (a2p - a1p) * sum((a2v-a1v)*(a2p-a1p)) / sum((a2p-a1p)**2)
     return v1prime, v2prime
 
+def chgmod(evt):
+    # print("press")
+    global stage, scene, observation, theory_low_T
+    k = evt.key
+    if k == "n" and stage <= 2:
+        stage += 1
+    
 
+scene.bind('keydown', chgmod)
+
+momentum = 0
+gamma = 5/3
+drawn = False
+v_W = 0
 while True:
     t += dt
     rate(10000)
@@ -78,9 +91,54 @@ while True:
     
     # collision between atoms and walls
     for i in range(N):
-        if abs(p_a[i][0]) >= L - size and p_a[i][0]*v_a[i][0] > 0:
-            v_a[i][0] = - v_a[i][0]
-        if abs(p_a[i][1]) >= L - size and p_a[i][1]*v_a[i][1] > 0:
+        if abs(p_a[i][0]) >= container.length/2 - size and p_a[i][0]*v_a[i][0] > 0:
+            delt_v = abs(v_a[i][0]) + (v_W)
+            v_a[i][0] = -np.sign(p_a[i][0])*(v_W + delt_v)
+            momentum += 2*abs(delt_v*m)
+        if abs(p_a[i][1]) >= container.height/2 - size and p_a[i][1]*v_a[i][1] > 0:
             v_a[i][1] = - v_a[i][1]
-        if abs(p_a[i][2]) >= L - size and p_a[i][2]*v_a[i][2] > 0:
+            momentum += 2*abs(v_a[i][1]*m)
+        if abs(p_a[i][2]) >= container.width/2 - size and p_a[i][2]*v_a[i][2] > 0:
             v_a[i][2] = - v_a[i][2]
+            momentum += 2*abs(v_a[i][2]*m)
+
+
+    if stage == 1:
+        v_W = L/(20000.0*dt)
+        container.length -= v_W*2*dt
+        if container.length <= L:
+            stage += 1
+
+    if stage == 2 and not drawn:
+        drawn = True
+        v_W = 0
+        observation = ghistogram(graph=vdist, bins=arange(
+                                 0., 4200., deltav), color=color.blue)
+        theory_low_T = gcurve(color=color.cyan)
+        dv = 10.
+        for v in arange(0., 4201.+dv, dv):
+            theory_low_T.plot(pos=(v, (deltav/dv)*N*4.*pi *
+                                ((m/(2.*pi*k*T))**1.5)*exp((-0.5*m*v**2)/(k*T))*(v**2)*dv))
+
+    if stage == 3:
+        container.length = 2*L
+
+    if (t//dt)%1000 == 0:
+        KE = 0.5*m*np.sum(np.square(v_a))
+        T = KE/((3/2)*N*k)
+        V = container.height*container.width*container.length
+        surface_area = 2*(container.height*container.width
+                          + container.height*container.length
+                          + container.length*container.width)
+        p = momentum/(1000*dt*surface_area)
+        print("============= Gas Data Report =============")
+        print(f"Stage       : {stage}")
+        print(f"Temperature : {T}")
+        print(f"Pressure    : {p}")
+        print(f"Volume      : {V}")
+        print(f"P*V         : {p*V}")
+        print(f"N*k*T       : {N*k*T}")
+        print(f"P*V^gamma   : {p*(V**gamma)}")
+        print("===========================================")
+        momentum = 0
+
--- hw6/histogram.py
+++ hw6/histogram.py
@@ -16,18 +16,18 @@ class ghistogram:
         for value in data:
             currentslots[min(
                 max(int((value - self.bins[0])/self.slotwidth), 0), self.slotnumber-1)] += 1
-            self.slots = (self.slots * self.n + currentslots)/(self.n + 1)
-            self.n += 1
-            if self.n == 1:
-                for (currentbin, barlength) in zip(self.bins, self.slots):
-                    self.bars.plot(pos=(currentbin, barlength))
-            else:
-                self.bars.data = list(zip(self.bins, self.slots))
+        self.slots = (self.slots * self.n + currentslots)/(self.n + 1)
+        self.n += 1
+        if self.n == 1:
+            for (currentbin, barlength) in zip(self.bins, self.slots):
+                self.bars.plot(pos=(currentbin, barlength))
+        else:
+            self.bars.data = list(zip(self.bins, self.slots))
 
 
 if __name__ == '__main__':
     vdist = vp.graph(width=450)
     observation = ghistogram(graph=vdist, bins=np.arange(1, 3, 0.5))
     observation.plot(data=[1.2, 2.3, 4])
-    observation.plot(data=[1, 1.7, 2.6])
-    observation.plot(data=[-0.5, 2, 2.3])
+    # observation.plot(data=[1, 1.7, 2.6])
+    # observation.plot(data=[-0.5, 2, 2.3])
